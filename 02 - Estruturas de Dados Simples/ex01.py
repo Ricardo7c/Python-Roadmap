5a737a4f6dd719667f3f592df906c2739bbc5284
Ricardo7c <Ricardo7c@hotmail.com>
2024-12-11 12:27:00 -0300

    Finalizado!

---
+++ 02 - Estruturas de Dados Simples/ex01.py
@@ -0,0 +1,8 @@
+# Cria uma lista com números inteiros
+numeros = [10, 20, 30, 40, 50]
+
+# Calcula a soma de todos os elementos da lista
+soma = sum(numeros)
+
+# Exibe o resultado
+print(f"A soma dos elementos da lista é: {soma}")
