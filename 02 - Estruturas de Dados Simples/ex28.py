83cdb380b750df42586a8fdc165252d0aa8c2ade
Ricardo7c <Ricardo7c@hotmail.com>
2024-12-12 23:08:42 -0300

    Finalizado!

---
+++ 02 - Estruturas de Dados Simples/ex28.py
@@ -0,0 +1,5 @@
+conjunto1 = {1, 2, 3, 4}
+conjunto2 = {3, 4}
+
+if conjunto2.issubset(conjunto2):
+    print("é um subset")
---
+++ 02 - Estruturas de Dados Simples/ex30.py
@@ -0,0 +1,5 @@
+meu_conjunto = {1, 2, 3, 4, 5}
+
+meu_conjunto.clear()
+
+print("Conjunto após clear():", meu_conjunto)
