88d67173fb69dd3038152d28b0409749ff593d4d
Ricardo7c <Ricardo7c@hotmail.com>
2024-12-14 11:44:09 -0300

    Finalizado!

---
+++ 04 - Bibliotecas e Arquivos/ex05.py
@@ -0,0 +1,10 @@
+import os;
+
+caminho = "04 - Bibliotecas e Arquivos"
+
+os.mkdir(f"{caminho}/novo_diretorio")
+
+for cada in os.listdir(caminho):
+    print(cada)
+
+os.rmdir(f"{caminho}/novo_diretorio")
