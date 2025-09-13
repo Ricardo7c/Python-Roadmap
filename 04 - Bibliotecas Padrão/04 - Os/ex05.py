de8166691292f0c7e95cb0de422554ce76409e51
Ricardo7c <Ricardo7c@hotmail.com>
2025-09-13 06:29:24 -0300

    Atualizado!

--- 04 - Bibliotecas Padrão/04 - Os/ex05.py
+++ 04 - Bibliotecas Padrão/04 - Os/ex05.py
@@ -3,4 +3,7 @@ import os
 arquivo = input("Digite o nome do arquivo/pasta: ")
 novo = input("Digite o novo nome: ")
 
+os.rename(arquivo, novo)
+
 print(f"'{arquivo}' foi renomeado para '{novo}'")
+
